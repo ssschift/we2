8dbf49a7eb34d4386c3cc5cc05b219413876e0ae
Rebecca <rdngangl@gmail.com>
2016-04-11 15:47:26 +0200

    display pictures out of the img-folder

--- content.php
+++ content.php
@@ -2,12 +2,35 @@
 <div id="uebersicht"> 
  
 	<?php			
-		$alledateien = scandir('img'); //Ordner "files" auslesen
+		$alledateien = scandir('img'); //Ordner "img" auslesen
+		 
+		$img_list = array();
 		 
 		foreach ($alledateien as $datei) { // Ausgabeschleife
-		   echo $datei."<br />"; //Ausgabe Einzeldatei
-		};	 
+		 	
+			$datei_name = substr ( $datei, -3, 3
+			);
+			
+			
+			if ($datei_name == "JPG"){
+				$img_list[] = "/img/".$datei;
+			}
+			
+			else {
+				continue;}
+		}
+
+		foreach($img_list AS $name) {
+			echo "<img src='.$name.' alt='' class='thumb'>";				;
+				
+		}
+	
+
+
+/* http://www.php-einfach.de/php-tutorial/php-array/ */
 	?>
 		
     </div> 	
 </p>
+
+
