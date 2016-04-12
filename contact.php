ad79c36392faf6eaf25d05d7487f5c9b21323f97
Fabian Sommer <fabian.sommer@mmp.htwchur.ch>
2016-04-12 13:01:58 +0200

    Tried out new way to position <nav>

---
+++ contact.php
@@ -0,0 +1,28 @@
+<html>
+ 
+ <?php include ("head.php"); ?> 
+ 
+	<body>	
+       <div id="wrapper">
+ 			<header>
+ 				<?php include ("header.php"); ?>
+ 			</header>
+            
+            <nav>
+				<?php include ("nav.php"); ?>
+  			</nav>
+             
+            <content>
+		 		<?php include ("contact_form.php"); ?>
+            </content> 
+ 				
+			<footer>	
+				<?php include ("footer.php"); ?>
+   			</footer>
+	        
+        </div>
+        
+    </body>
+    
+</html>
+    
