a5ceab4aab43759e51f1df000ea8612905752a97
Rebecca <rdngangl@gmail.com>
2016-04-18 16:06:28 +0200

    no message

--- contact.php
+++ contact.php
@@ -1,28 +1,93 @@
-<html>
- 
- <?php include ("head.php"); ?> 
- 
-	<body>	
-       <div id="wrapper">
- 			<header>
- 				<?php include ("header.php"); ?>
- 			</header>
-            
-            <nav>
-				<?php include ("nav.php"); ?>
-  			</nav>
-             
+ <?php include ("header.php"); ?>
             <content>
-		 		<?php include ("contact_form.php"); ?>
+            	
+<h1>Kontakt</h1>
+	<!-- ANREDE -->
+		<div class="input">
+			<label class="field required">Anrede*: 
+            	<form method="POST" action="form.php">
+        			<input type="radio" name="gender" value="f">Frau
+                    <input type="radio" name="gender" value="m">Mann </label>
+        		</form>
+        </div>
+	<!-- VORNAME -->
+		<div class="input">
+            	<form method="POST" action="form.php">
+                	<label for="vorname" class="field required">Vorname*:
+        			<input type="text" id="vorname" name=vorname></label>
+        		</form>
+		</div>
+	<!-- NACHNAME -->
+		<div class="input">
+			<label class="field required">Nachname*:
+            	<form method="POST" action="form.php">
+        			<input type="text" name=nachname></label>
+        		</form>
+		</div>
+	<!-- E-MAIL -->
+		<div class="input">
+			<label class="field required">E-Mail*:
+            	<form method="POST" action="form.php">
+        			<input type="mail" name=email></label>
+        		</form>
+		</div>
+	<!-- TEL -->
+		<div class="input">
+			<label class="field">Telefon:
+            	<form method="POST" action="form.php">
+        			<input type="text" name=telefon></label>
+        		</form>
+		</div>
+	<!-- STRASSE -->
+		<div class="input">
+			<label class="field">Strasse:
+            	<form method="POST" action="form.php">
+        			<input type="text" name=strasse></label>
+        		</form>
+		</div>
+	<!-- PLZ -->
+		<div class="input">
+			<label class="field">PLZ:
+            	<form method="POST" action="form.php">
+        			<input type="text" name=plz></label>
+        		</form>
+		</div>
+	<!-- ORT -->
+		<div class="input">
+			<label class="field">Ort:
+            	<form method="POST" action="form.php">
+        			<input type="text" name=ort></label>
+        		</form>
+		</div>
+	<!-- LAND -->
+		<div class="input">
+			<label class="field">Land: 
+            	<form method="POST" action="form.php">
+        			<input type="text" name=land></label>
+        		</form>
+		</div>
+	<!-- BROSCH -->
+		<div class="input">
+			<form method="POST" action="form.php">
+        		<input type="checkbox" name="broschuere" value="">Bitte senden Sie mir eine Broschüre zu.
+        	</form>
+		</div>
+	<!-- KOMMENTAR -->
+		<div class="input">
+			<label class="field">Kommentar:
+            	<form method="POST" action="form.php">
+					<textarea rows="3" cols="30">
+					</textarea></label>
+        		</form>
+	<!-- SUBMIT -->
+		<div class="submit">
+			<input type="submit" value="abschicken" ></div>
+                
+                
+                
             </content> 
  				
-			<footer>	
 				<?php include ("footer.php"); ?>
-   			</footer>
 	        
-        </div>
-        
-    </body>
-    
-</html>
+
     
---
+++ slide.php
@@ -0,0 +1,82 @@
+<?php include ("header.php"); ?>
+                  
+  <content>
+
+ <!-- Carousel -->
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+<!-- Carousel Indicators -->
+<li data-target="#myCarousel" data-slide-to="1"></li>
+
+<!-- Carousel Controls -->
+<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="img/1_116.jpg" alt="Chania">
+      <div class="carousel-caption">
+        <h3>Chania</h3>
+        <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="img/006.jpg" alt="Chania">
+      <div class="carousel-caption">
+        <h3>Chania</h3>
+        <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="img/050.jpg" alt="Flower">
+      <div class="carousel-caption">
+        <h3>Flowers</h3>
+        <p>Beatiful flowers in Kolymbari, Crete.</p>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="img/040.jpg" alt="Flower">
+      <div class="carousel-caption">
+        <h3>Flowers</h3>
+        <p>Beatiful flowers in Kolymbari, Crete.</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+
+
+
+
+
+
+        
+    </content> 
+ 			
+
+
+<?php include ("footer.php"); ?>
