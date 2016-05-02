64fe6fdb70dca950686951fa83f530d08ac298df
Fabian Sommer <fabian.sommer@mmp.htwchur.ch>
2016-05-02 17:13:23 +0200

    Integrated icons top right

--- header.php
+++ header.php
@@ -11,22 +11,26 @@
 	<header>
 		<div id="nav1">
 			<ul>
-				<li><a href="preise.php">Preise</a></li>
-				<li><a href="about.php">About</a></li>
-				<li><a href="contact.php">Kontakt</a></li>
+				<li><a href="preise.php"><img src="img/icons/facebook.svg" alt="Logo Facebook"></a></li>
+				<li><a href="about.php"><img src="img/icons/about.svg" alt="Logo About"></a></li>
+				<li><a href="contact.php"><img src="img/icons/contact.svg" alt="Logo Contact"></a></li>
 			</ul>
 		</div>
 		<br>
 
-			  <div><a href="http://localhost/fotograph"><img src="img/L_Atelier_Lightning.svg" alt="Logo Atelier Lightning" class="logo"></a></div>
+        <div>
+            <a href="http://localhost:8888/atelier_lightning">
+                <img src="img/L_Atelier_Lightning.svg" alt="Logo Atelier Lightning" class="logo">
+            </a>
+        </div>
 
-			<div id="nav2">
-				<ul >
-					<li><a href="atelier.php" <?php if($fileName == "/atelier.php") echo 'id="aktiv"'; ?>>Atelier</a></li>
-					<li><a href="indoor.php" <?php if($fileName == "/indoor.php") echo 'id="aktiv"'; ?>>Indoor</a></li>
-					<li><a href="outdoor.php" <?php if($fileName == "/outdoor.php") echo 'id="aktiv"'; ?>>Outdoor</a></li>
-				</ul>
-			</div>
+        <div id="nav2">
+		  <ul>
+			<li><a href="atelier.php" <?php if($fileName == "/atelier.php") echo 'id="aktiv"'; ?>>Atelier</a></li>
+			<li><a href="indoor.php" <?php if($fileName == "/indoor.php") echo 'id="aktiv"'; ?>>Indoor</a></li>
+			<li><a href="outdoor.php" <?php if($fileName == "/outdoor.php") echo 'id="aktiv"'; ?>>Outdoor</a></li>
+		  </ul>
+		</div>
 
 </header>
 
